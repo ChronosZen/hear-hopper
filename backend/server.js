cc8808228a2c1a097e13c89cb08f321ecc7c3208
ChronosZen <krisana9852@gmail.com>
2024-02-24 16:48:06 -0800

    Update frontend router and backend port

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import {connectDB} from './config/db.js';
-import userRouter from './routes/User.js';
-import resultRouter from './routes/Result.js';
-import resourcesRouter from './routes/Resource.js';
+import express from "express";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/User.js";
+import resultRouter from "./routes/Result.js";
+import resourcesRouter from "./routes/Resource.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
@@ -14,15 +14,15 @@ app.use(express.json());
 connectDB();
 
 // Routes
-app.use('/users', userRouter);
-app.use('/results', resultRouter);
-app.use('/resources', resourcesRouter);
+app.use("/users", userRouter);
+app.use("/results", resultRouter);
+app.use("/resources", resourcesRouter);
 // app.use('/noisecheck', NoiseCheck);
 // app.use('/hearingage', HearingAge);
 
 // Root route
-app.get('/', (req, res) => {
-  res.send('Hello, HearHopper!');
+app.get("/", (req, res) => {
+  res.send("Hello, HearHopper!");
 });
 
 // Start server
