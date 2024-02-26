5d62dcc7d069bc638d3eec1baaaaf8facf024626
ChronosZen <krisana9852@gmail.com>
2024-02-26 00:16:11 -0800

    implement tabstack for profile navigation

--- frontend/src/routes/BottomTab.jsx
+++ frontend/src/routes/BottomTab.jsx
@@ -6,14 +6,31 @@ import ProfileScreen from "../screens/ProfileScreen";
 import TestScreen from "../screens/TestScreen";
 import TrainScreen from "../screens/TrainScreen";
 import ExampleScreen from "../screens/ExampleScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AddProfileScreen from "../screens/AddProfileScreen";
+
+const ProfileStack = createNativeStackNavigator();
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator screenOptions={{ headerShown: true }}>
+      <ProfileStack.Screen
+        name="MainProfile"
+        component={ProfileScreen}></ProfileStack.Screen>
+      <ProfileStack.Screen
+        name="AddProfile"
+        component={AddProfileScreen}></ProfileStack.Screen>
+    </ProfileStack.Navigator>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 const BottomTab = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Test" component={TestScreen} />
       <Tab.Screen name="Train" component={TrainScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Profile" component={ProfileStackScreen} />
       <Tab.Screen name="Example" component={ExampleScreen} />
     </Tab.Navigator>
   );
