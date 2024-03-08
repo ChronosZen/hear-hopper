e3fad34df2d8b706a72ad9346894d4de80355b0c
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2024-03-08 12:41:44 -0800

    adjust UI on HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,7 @@
 import { StyleSheet, View } from "react-native";
+import ButtonFunc from "../components/reusable/ButtonFunc";
 import React from "react";
+import { Colors } from "../styles";
 import TestResult from "../components/TestResult";
 import {
   HStack,
@@ -7,65 +9,124 @@ import {
   Heading,
   Text,
   Link,
-  Image
+  Image,
+  Button,
+  SafeAreaView,
+  GluestackUIProvider,
+  Box,
+  ScrollView,
+  Card,
+  Pressable,
+  Center,
+  Icon,
+  ChevronRightIcon,
+  LinkText,
+
 } from '@gluestack-ui/themed';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
+
+  const onClick = () => {
+    console.log("card is clicked.")
+  }
+
+
+  const homeCards = [{
+    title: "Child Ear Training"
+  },
+  {
+    title: "Parental Control"
+  }]
+
   return (
+
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ScrollView>
+
+      </ScrollView>
       {/* Header */}
-      <View>
+      <Box minWidth={"$full"} justifyContent="space-between">
         <HStack>
-          <VStack>
+          <VStack >
             <Heading>Welcome</Heading>
             <Heading> User name</Heading>
           </VStack>
-          <Image 
+          <Image
             size="md"
             borderRadius="$none"
+            alt="mascot"
             source={{
               uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
             }} />
         </HStack>
-      </View>
+      </Box>
 
       {/* Hearing result section */}
-      <View>
-        <VStack>
-          <HStack>
-            <Heading size={"3xl"}>Hearing Test Result</Heading>
-            <Link>View all</Link>
-          </HStack>
-          <TestResult />
-        </VStack>
-      </View>
+
+      <VStack>
+        <HStack justifyContent="space-between" alignItems="center" margin={16} space="2xl">
+          <Heading size={"xl"}>Hearing Test Result</Heading>
+          <Link>
+            <LinkText>View all</LinkText>
+          </Link>
+        </HStack>
+        <Card>
+          
+            <Image
+              size="md"
+              borderRadius="$none"
+              alt="mascot"
+              source={{
+                uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+              }} />
+            <VStack>
+              <Heading>Hearing Level</Heading>
+              <Text>Your child took the Hearing test on 8th March, 2024</Text>
+              <Link><LinkText>View Report</LinkText></Link>
+            </VStack>
+          
+        </Card>
+        {/* <TestResult /> */}
+      </VStack>
 
       {/* Hearing test section */}
-      <View>
-        <HStack>
-          <VStack>
-            <Heading>Test your child hearing</Heading>
-            <Text>Our hearing test is created keeping your child\'s ear sensitivity in mind.</Text>
-            <Button>Take the Test</Button>
-          </VStack>
-          <Image size="md"
+      <HStack margin={16} backgroundColor="pink" space="xl">
+        <VStack margin={16} borderColor="black" borderWidth={1}>
+          <Heading>Test your child hearing</Heading>
+          <Text>Our hearing test is created keeping your child\'s ear sensitivity in mind.</Text>
+          <ButtonFunc text={"Take the test"} handleOnPress={()=> navigation.navigate("TestScreen")} >
+          </ButtonFunc>
+        </VStack>
+        <Text margin={16}>Image</Text>
+        {/* <Image size="md"
             borderRadius="$none"
             source={{
-              uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-            }} />
-        </HStack>
-      </View>
+              uri: "https://picsum.photos/seed/picsum/200/300",
+            }}
+             /> */}
+      </HStack>
 
       {/* Cards (Ear training & Parental control) section */}
-      <View>
-          <View>
-            <Image></Image>
-            <Heading>Child Ear Training</Heading>
-            <Text>Lorem Ipsum is simply dummy text of the printing and typesetting.</Text>
+      {/* <Image></Image> */}
+
+      {homeCards.map((homeCard, index) => {
+        return (
+          <View key={index}>
+            <Pressable onPress={onClick}>
+              <Card margin={16} borderWidth={1} borderColor={Colors.primary.p1} backgroundColor={Colors.primary.p5}  >
+                <HStack justifyContent="space-between" >
+                  <Heading color={Colors.primary.p1}>{homeCard.title}</Heading>
+                  <Icon as={ChevronRightIcon} m="$2" w="$4" h="$4" color={Colors.primary.p1} />
+                </HStack>
+              </Card>
+            </Pressable>
           </View>
-      </View>
+        )
+      })}
+
     </View>
   );
+
 };
 
 export default HomeScreen;
