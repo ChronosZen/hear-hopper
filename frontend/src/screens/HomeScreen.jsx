74495cef5c23e99f5592dc33873b20909198a50a
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2024-03-08 17:12:01 -0800

    finish HomeScreen function
    Will need to implement routing once other pages are done. (Latest test result, test results and  Parental control page. )

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import ButtonFunc from "../components/reusable/ButtonFunc";
 import React from "react";
 import { Colors } from "../styles";
 import TestResult from "../components/TestResult";
+
 import {
   HStack,
   VStack,
@@ -10,26 +11,27 @@ import {
   Text,
   Link,
   Image,
-  Button,
-  SafeAreaView,
-  GluestackUIProvider,
   Box,
   ScrollView,
   Card,
   Pressable,
-  Center,
   Icon,
   ChevronRightIcon,
   LinkText,
-
+  SafeAreaView
 } from '@gluestack-ui/themed';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({navigation, route, userData}) => {
+
+  console.log("navigation", navigation);
+  console.log("route", route);
+  console.log("userData", userData);
+  // const { userData } = route.param;
+  // console.log("here is the userData from HomeScreen", userData);
 
   const onClick = () => {
     console.log("card is clicked.")
   }
-
 
   const homeCards = [{
     title: "Child Ear Training"
@@ -40,16 +42,17 @@ const HomeScreen = ({navigation}) => {
 
   return (
 
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <ScrollView>
 
       </ScrollView>
       {/* Header */}
-      <Box minWidth={"$full"} justifyContent="space-between">
-        <HStack>
+      <Box minWidth={"$full"} backgroundColor={Colors.primary.p1}>
+        <HStack justifyContent="space-between" alignItems="center" paddingHorizontal={16}>
           <VStack >
-            <Heading>Welcome</Heading>
-            <Heading> User name</Heading>
+            <Heading color="white">Welcome</Heading>
+            <Heading color="white">{userData.firstName}</Heading>
           </VStack>
           <Image
             size="md"
@@ -62,42 +65,39 @@ const HomeScreen = ({navigation}) => {
       </Box>
 
       {/* Hearing result section */}
-
       <VStack>
         <HStack justifyContent="space-between" alignItems="center" margin={16} space="2xl">
-          <Heading size={"xl"}>Hearing Test Result</Heading>
-          <Link>
+          <Heading size={"lg"}>Hearing Test Result</Heading>
+          <Link onPress={()=>navigation.navigate("TestResult")}>
             <LinkText>View all</LinkText>
           </Link>
         </HStack>
-        <Card>
-          
-            <Image
+        <Card backgroundColor={Colors.secondary.g6} borderWidth={2} borderColor={Colors.secondary.g2} >
+            {/* <Image
               size="md"
               borderRadius="$none"
               alt="mascot"
               source={{
                 uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-              }} />
+              }} /> */}
             <VStack>
-              <Heading>Hearing Level</Heading>
+              <Heading color={Colors.secondary.g1}>Excellent Hearing</Heading>
               <Text>Your child took the Hearing test on 8th March, 2024</Text>
               <Link><LinkText>View Report</LinkText></Link>
-            </VStack>
-          
+            </VStack>          
         </Card>
         {/* <TestResult /> */}
       </VStack>
 
       {/* Hearing test section */}
-      <HStack margin={16} backgroundColor="pink" space="xl">
-        <VStack margin={16} borderColor="black" borderWidth={1}>
+      <HStack margin={16} space="xl">
+        <VStack margin={16}>
           <Heading>Test your child hearing</Heading>
           <Text>Our hearing test is created keeping your child\'s ear sensitivity in mind.</Text>
-          <ButtonFunc text={"Take the test"} handleOnPress={()=> navigation.navigate("TestScreen")} >
+          <ButtonFunc text={"Take the test"} handleOnPress={()=> navigation.navigate("Test")} >
           </ButtonFunc>
         </VStack>
-        <Text margin={16}>Image</Text>
+
         {/* <Image size="md"
             borderRadius="$none"
             source={{
@@ -111,9 +111,9 @@ const HomeScreen = ({navigation}) => {
 
       {homeCards.map((homeCard, index) => {
         return (
-          <View key={index}>
-            <Pressable onPress={onClick}>
-              <Card margin={16} borderWidth={1} borderColor={Colors.primary.p1} backgroundColor={Colors.primary.p5}  >
+          <View>
+            <Pressable onPress={()=>{navigation.navigate("Train")}}>
+              <Card key={index} margin={16} borderWidth={1} borderColor={Colors.primary.p1} backgroundColor={Colors.primary.p5}  >
                 <HStack justifyContent="space-between" >
                   <Heading color={Colors.primary.p1}>{homeCard.title}</Heading>
                   <Icon as={ChevronRightIcon} m="$2" w="$4" h="$4" color={Colors.primary.p1} />
@@ -123,8 +123,7 @@ const HomeScreen = ({navigation}) => {
           </View>
         )
       })}
-
-    </View>
+    </SafeAreaView>
   );
 
 };
