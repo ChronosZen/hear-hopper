5191a9cde1975db2649856fce43aa15d3c918689
ChronosZen <krisana9852@gmail.com>
2024-03-08 16:29:58 -0800

    Add basic training journey

--- frontend/src/components/train/ProgressBar.jsx
+++ frontend/src/components/train/ProgressBar.jsx
@@ -1,10 +1,22 @@
+import {
+  HStack,
+  Progress,
+  ProgressFilledTrack,
+  VStack,
+} from "@gluestack-ui/themed";
 import { Text, View } from "react-native";
-
+import { Colors } from "../../styles";
 const ProgressBar = () => {
   return (
-    <View>
-      <Text>ProgressBar</Text>
-    </View>
+    <VStack space="md" marginBottom={48}>
+      <Progress value={20} w="100%" h={12} bg={Colors.gs.gs6}>
+        <ProgressFilledTrack h={12} bg={Colors.gs.black} />
+      </Progress>
+      <HStack justifyContent="space-between" width={"100%"}>
+        <Text>Progress</Text>
+        <Text>01/05</Text>
+      </HStack>
+    </VStack>
   );
 };
 
