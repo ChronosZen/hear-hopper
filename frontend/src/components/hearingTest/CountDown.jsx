fd3ddfde95debdf865f379ad4c2f404aeb4990ae
hridyauk <h180@mylangara.ca>
2024-03-11 23:31:48 -0700

    countdown

--- frontend/src/components/hearingTest/CountDown.jsx
+++ frontend/src/components/hearingTest/CountDown.jsx
@@ -3,49 +3,37 @@ import { Audio } from 'expo-av';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 import AudioGenerator from './AudioGenerator';
 
-// const countDownReducer = (state, action) => {
-//     switch(action.type){
-//         case 'dec':
-//             return {count: state.count-1}
-//         default:
-//             return state
-//     }
-// }
+const countDownReducer = (state, action) => {
+    switch(action.type){
+        case 'dec':
+            return {count: state.count-1}
+        default:
+            return state
+    }
+}
 
-const CountDown = ({count}) => {
-    // const [state, dispatch] = useReducer(countDownReducer, {count: 3})
+const CountDown = ({navigation}) => {
+    const [state, dispatch] = useReducer(countDownReducer, {count: 3})
 
-    // useEffect(() => {
-    //     if(state.count>0){
-    //         const interval = setInterval(() => {
-    //             dispatch({type: 'dec'})
+    useEffect(() => {
+        if(state.count>0){
+            const interval = setInterval(() => {
+                dispatch({type: 'dec'})
 
-    //             if(state.count===1){
-    //                 clearInterval(interval)
-    //                 playSound()
-    //             }
+                if(state.count===1){
+                    clearInterval(interval)
+                    // playSound()
+                }
 
-    //         }, 1000);
-    //     }
-    // }, [])
+            }, 1000);
+        }
+    }, [])
 
     return (
         <View style={{flex:1, justifyContent: "center", alignItems: "center" }}>
             <Text style={{ textAlign: "center" }}>Test starts in</Text>
-            <Text style={styles.countDownText}>{count}</Text>
+            <Text style={styles.countDownText}>{state.count}</Text>
         </View>
-        // <View style={styles.container} >
-        //     {state.count>0 ? 
-        //         <View>
-        //             <Text style={{textAlign: 'center'}}>Test starts in</Text>
-        //             <Text style={styles.countDownText} >{state.count}</Text>
-        //         </View> 
-        //     :   <View>
-        //             <AudioGenerator />
-        //         </View>
-        //     }
-            
-        // </View>
     )
 };
 
