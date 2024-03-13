ae7c760eda77e3427ebd3b52030af823b535843e
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2024-03-13 10:48:40 -0700

    add stackScreen for ParentalControlScreen
    krisana@gmail.com

--- frontend/src/components/user/NoiseChecker.jsx
+++ frontend/src/components/user/NoiseChecker.jsx
@@ -4,7 +4,6 @@ import { View } from "react-native";
 import ButtonFunc from "../reusable/ButtonFunc";
 import { Colors } from "../../styles";
 import { UserPermissions } from "./UserPermissions"
-
 import {
     Heading,
     Text,
@@ -61,39 +60,39 @@ const NoiseChecker = () => {
     // console.log("Message from NoiseChecker component.") -> OK
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const { checkPermissions, requestPermissions } = UserPermissions()
+    // const { checkPermissions, requestPermissions } = UserPermissions()
 
     // Check microphone access and recording permission status.
-    // const checkPermissions = async () => {
-    //     try {
-    //         const microphonePermission = await Audio.getPermissionsAsync()
-    //         dispatch({ type: "SET_MICROPHONE_PERM_GRANTED", payload: microphonePermission.status === "granted" })
-    //         // console.log("microphone Permissions -> ", microphonePermission.status) // -> OK
+    const checkPermissions = async () => {
+        try {
+            const microphonePermission = await Audio.getPermissionsAsync()
+            dispatch({ type: "SET_MICROPHONE_PERM_GRANTED", payload: microphonePermission.status === "granted" })
+            // console.log("microphone Permissions -> ", microphonePermission.status) // -> OK
 
-    //         const recordingPermission = await Audio.getPermissionsAsync()
-    //         dispatch({ type: "SET_RECORDING_PERM_GRANTED", payload: recordingPermission.status === "granted" })
-    //         // console.log("recording Permission -> ", recordingPermission.status) // -> OK
-    //     } catch (error) {
-    //         console.error("checkPermissions is Error: ", error)
-    //     }
-    // }
+            const recordingPermission = await Audio.getPermissionsAsync()
+            dispatch({ type: "SET_RECORDING_PERM_GRANTED", payload: recordingPermission.status === "granted" })
+            // console.log("recording Permission -> ", recordingPermission.status) // -> OK
+        } catch (error) {
+            console.error("checkPermissions is Error: ", error)
+        }
+    }
 
     // Request microphone and recording access to the first time user or previous denying user .
-    // const requestPermissions = async () => {
-    //     try {
-    //         if (!state.isMicrophonePermGranted) {
-    //             const newMicrophonePermission = await Audio.requestPermissionsAsync()
-    //             dispatch({ type: "SET_MICROPHONE_PERM_GRANTED", payload: newMicrophonePermission.status === "granted" })
-    //         }
+    const requestPermissions = async () => {
+        try {
+            if (!state.isMicrophonePermGranted) {
+                const newMicrophonePermission = await Audio.requestPermissionsAsync()
+                dispatch({ type: "SET_MICROPHONE_PERM_GRANTED", payload: newMicrophonePermission.status === "granted" })
+            }
 
-    //         if (state.isRecordingPermGranted) {
-    //             const newRecordingPermission = await Audio.requestPermissionsAsync()
-    //             dispatch({ type: "SET_RECORDING_PERM_GRANTED", payload: newRecordingPermission.status === "granted" })
-    //         }
-    //     } catch (error) {
-    //         console.error("requestPermissions is Error: ", error)
-    //     }
-    // }
+            if (state.isRecordingPermGranted) {
+                const newRecordingPermission = await Audio.requestPermissionsAsync()
+                dispatch({ type: "SET_RECORDING_PERM_GRANTED", payload: newRecordingPermission.status === "granted" })
+            }
+        } catch (error) {
+            console.error("requestPermissions is Error: ", error)
+        }
+    }
 
 
     let noiseCheckInterval = null
---
+++ frontend/src/screens/ParentalControlScreen.jsx
@@ -0,0 +1,17 @@
+import HeaderText from "../components/reusable/HeaderText"
+
+const ParentalControlScreen = () => {
+
+    return (
+        <>
+            <HeaderText text="Parental Control" />
+
+        </>
+
+    )
+
+
+
+}
+
+export default ParentalControlScreen
