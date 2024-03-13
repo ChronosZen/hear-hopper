54fc9d032ac67dcc3b3e250b83c6b9a240d8f25e
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2024-03-13 16:11:51 -0700

    Update status codes for Backend responses to be more descriptive

--- backend-rewrite/src/handlers/kid.ts
+++ backend-rewrite/src/handlers/kid.ts
@@ -1,14 +1,15 @@
 import { Kid } from "../schema/schema";
 
-export const registerKid = async (req,res) => {
-    const newKid = await Kid.create({
-        firstName: req.body.firstName,
-        birthYear: req.body.birthYear,
-        gender: req.body.gender,
-        hearingAid: req.body.hearingAid,
-        image: req.body.image,
-        parent: req.user.id
-    })
+export const registerKid = async (req, res) => {
+  const newKid = await Kid.create({
+    firstName: req.body.firstName,
+    birthYear: req.body.birthYear,
+    gender: req.body.gender,
+    hearingAid: req.body.hearingAid,
+    image: req.body.image,
+    parent: req.user.id,
+  });
 
-    res.json({data: newKid})
-}
+  res.status(201);
+  res.json({ data: newKid });
+};
