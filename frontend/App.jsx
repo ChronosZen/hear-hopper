e071db28a560d0be00445b35e8a34e564680f6d2
Kimitoshi Nakaya <knakaya00@mylangara.ca>
2024-03-15 21:20:19 -0700

    Get test result by creating a component and show on UI

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -29,7 +29,7 @@ import AddProfileScreen from "./src/screens/AddProfileScreen";
 const queryClient = new QueryClient()
 const Stack = createNativeStackNavigator();
 export default function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(true);
   const [userData, setUserData] = useState({});
   let [fontsLoaded, fontError] = useFonts({
     Outfit_400Regular,
