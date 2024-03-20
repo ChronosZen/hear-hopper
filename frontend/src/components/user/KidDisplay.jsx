45e2b353c4afc9c57a445212027752a6d0c81891
AnhPhanNguyenLangara <131826328+AnhPhanNguyenLangara@users.noreply.github.com>
2024-03-20 16:55:22 -0700

    Add placeholder avatar for kid without image

--- frontend/src/components/user/KidDisplay.jsx
+++ frontend/src/components/user/KidDisplay.jsx
@@ -1,10 +1,11 @@
 import { HStack, Image } from "@gluestack-ui/themed";
 import { Text, View } from "react-native";
+import placeHolder from "../../../assets/PlaceholderAvatar.jpg"
 
 const KidDisplay = ({ image, childName }) => {
   return (
     <HStack gap={16} maxHeight={250} justifyContent="start" alignItems="center">
-      <Image size="sm" borderRadius="$full" source={image} alt={childName} />
+      <Image size="sm" borderRadius="$full" source={image?image:placeHolder} alt={childName} />
       <Text>{childName}</Text>
     </HStack>
   );
