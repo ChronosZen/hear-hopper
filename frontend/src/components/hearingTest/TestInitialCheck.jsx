8cf1d1a319043a7ba528436527ae3a8533863a09
hridyauk <h180@mylangara.ca>
2024-03-20 14:49:17 -0700

    styling changes

--- frontend/src/components/hearingTest/TestInitialCheck.jsx
+++ frontend/src/components/hearingTest/TestInitialCheck.jsx
@@ -12,6 +12,7 @@ const TestInitialCheck = ({navigation}) => {
     function changeSection(){
         if(setting === 1){
             navigation.navigate("Noise Check")
+            setSetting(0)
         }
         if(!setting){
             setSetting(1)
@@ -19,29 +20,31 @@ const TestInitialCheck = ({navigation}) => {
     }
 
     return (
-        <VStack style={{flex: 1}}>
+        <VStack flex={1}>
             <VStack style={styles.container}>
                 <SVG xml={mainMastcot} width="180" height="180" />
                 {setting ?
                     <VStack style={{justifyContent: 'space-between', alignItems: 'start', gap: 32}}>
                         <Text style={{...Typography.heading.h4}}>Please make sure to Turn Off</Text>
-                        <HStack marginHorizontal={56}>
+                        <HStack marginHorizontal={78}>
                             <SVG xml={notification} width="24" height="24" />
                             <Text>  Notifications</Text>
                         </HStack>
-                        <HStack marginHorizontal={56}>
+                        <HStack marginHorizontal={78}>
                             <SVG xml={soundIcon} width="24" height="24" />
                             <Text>  Sound effects</Text>
                         </HStack>
                     </VStack>
                     :
-                    <VStack style={{alignItems: 'center'}}>
-                        <Text style={{...Typography.heading.h4, textAlign: 'center'}}>Please wear a Headphone for Accurate Results</Text>
+                    <>
+                    <Text style={{...Typography.heading.h4, textAlign: 'center'}}>Please wear a Headphone for Accurate Results</Text>
+                    <VStack flex={1} alignItems= 'center' justifyContent= 'center'>
                         <SVG xml={testIcon} width="120" height="120" fill={Colors.primary.p1} />
                     </VStack>
+                    </>
                     }
             </VStack>
-            <View>
+            <View style={{marginBottom: 32, marginHorizontal: 24}}>
                 <ButtonFunc text="Proceed" handleOnPress={changeSection} />
             </View>
         </VStack>
@@ -53,7 +56,6 @@ export default TestInitialCheck;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-evenly',
     alignItems: 'center',
     marginBottom: 24, 
     marginHorizontal: 32
