a0695b23a2b4de733b42d4fa679b63caa261c2ec
hridyauk <h180@mylangara.ca>
2024-03-23 12:17:19 -0700

    ui updates

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -8,6 +8,14 @@ import KidDisplay from "../components/user/KidDisplay";
 import { useQuery } from "@tanstack/react-query";
 import * as secureStorage from "expo-secure-store";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, SafeAreaView, VStack, HStack, ScrollView } from "@gluestack-ui/themed";
+import Dad from "../../assets/dad.jpg";
+import KidDisplay from "../components/user/KidDisplay";
+import { useQuery } from "@tanstack/react-query";
+import * as secureStorage from 'expo-secure-store';
+import SVG from "../components/svg/SVG";
+import { ear } from "../components/svg/svgs";
+import { Typography } from "../styles/index";
 
 ProfileScreen = ({ navigation, route }) => {
   const { isPending, error, data } = useQuery({
@@ -41,36 +49,66 @@ ProfileScreen = ({ navigation, route }) => {
   }
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <VStack justifyContent="center" alignItems="center" gap={8}>
-        <HeaderText text="Profile" />
-        <Text>Hello, {userData.firstName}</Text>
-        <Image size="lg" borderRadius="$full" alt="test" source={Dad} />
-      </VStack>
-      <VStack justifyContent="center" alignItems="start" gap={8}>
-        {kidArr.map((kid) => (
-          <KidDisplay
-            image={kid.image}
-            childName={kid.firstName}
-            key={kid.id}
-          />
-        ))}
-      </VStack>
-      <View style={{ flex: 1, alignItems: "center", gap: 32 }}>
-        <ButtonFunc
-          text="Add +"
-          handleOnPress={() => {
-            navigateAddProfile();
-          }}
-        />
-        <View style={{ flex: 1 }}>
-          {/* <ButtonFunc
-            text="View Sample"
+      <ScrollView >
+        <VStack justifyContent="center" alignItems="center" gap={8}>
+          <HeaderText text="Profile" />
+          <Text>Hello, {userData.firstName}</Text>
+          <Image size="lg" borderRadius="$full" alt="test" source={Dad} />
+        </VStack>
+
+        <VStack justifyContent="center" alignItems="start" gap={8}>
+          {kidArr.map((kid) => (
+            <KidDisplay
+              image={kid.image}
+              childName={kid.firstName}
+              key={kid.id}
+            />
+          ))}
+        </VStack>
+
+        <VStack alignItems="center" marginBottom={12}>
+          <ButtonFunc
+            text="Add +"
             handleOnPress={() => {
-              navigateSample();
+              navigateAddProfile();
             }}
-          /> */}
-        </View>
-      </View>
+          />
+        </VStack>
+        {/* <View style={{ flex: 1, alignItems: "center", gap: 32 }}>
+          
+          <View style={{ flex: 1 }}> */}
+            {/* <ButtonFunc
+              text="View Sample"
+              handleOnPress={() => {
+                navigateSample();
+              }}
+            /> */}
+          {/* </View>
+        </View> */}
+
+        <VStack gap={22}>
+            <HStack alignItems="center">
+              <SVG xml={ear} width="24" height="24" />
+              <Text style={Typography.body.bl}>Change Password</Text>
+            </HStack>
+            <HStack alignItems="center">
+              <SVG xml={ear} width="24" height="24" />
+              <Text style={Typography.body.bl}>Privacy Policy</Text>
+            </HStack>
+            <HStack alignItems="center">
+              <SVG xml={ear} width="24" height="24" />
+              <Text style={Typography.body.bl}>Terms of Use</Text>
+            </HStack>
+            <HStack alignItems="center">
+              <SVG xml={ear} width="24" height="24" />
+              <Text style={Typography.body.bl}>Help</Text>
+            </HStack>
+            <HStack alignItems="center">
+              <SVG xml={ear} width="24" height="24" />
+              <Text style={Typography.body.bl}>Logout</Text>
+            </HStack>
+          </VStack>
+      </ScrollView>
     </SafeAreaView>
   );
 };
