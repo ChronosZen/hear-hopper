b771bc2f1444800772d9be045e3752a96bd29a12
hridyauk <h180@mylangara.ca>
2024-03-29 21:46:59 -0700

    updated animation for tutorial

--- frontend/src/components/hearingTest/TestTutorial.jsx
+++ frontend/src/components/hearingTest/TestTutorial.jsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
-import { useState, useEffect, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
 // import CountDown from "../components/hearingTest/CountDown";
 import ButtonFunc from "../reusable/ButtonFunc";
 import { Typography, Colors } from '../../styles/index';
 import { VStack, HStack } from "@gluestack-ui/themed";
+import { testEar, heldTestBtn, testBtn } from "../svg/svgs";
+import HeaderText from "../reusable/HeaderText";
+import CloseButton from "../reusable/CloseButton";
+import AnimatedLottieView from 'lottie-react-native'
 import SVG from "../svg/SVG";
-import { testIcon, ear } from "../svg/svgs";
+
 // import CountDown from "./CountDown";
 
 const countDownReducer = (state, action) => {
@@ -20,6 +24,7 @@ const countDownReducer = (state, action) => {
 const TestTutorial = ({navigation}) => {
     const [state, dispatch] = useReducer(countDownReducer, {count: 3})
     const [showCountDown, setShowCountDown] = useState(false)
+    const [btnAnimate, setBtnAnimate] = useState(false)
     
     const startCountDown = () => {
         setShowCountDown(true)
@@ -27,24 +32,43 @@ const TestTutorial = ({navigation}) => {
         navigation.navigate("Ear Test")
         setShowCountDown(false)
     }
+
+    useEffect(() => {
+        const btnReleased = setInterval(() => {
+            setBtnAnimate(true)
+        }, 1000)
+
+        const btnPressed = setInterval(() => {
+            setBtnAnimate(false)
+        }, 4000)
+
+        return () => {
+            clearInterval(btnReleased)
+            clearInterval(btnPressed)
+        }
+    },[])
   
     return (
-        <>
+        <VStack flex={1} m={24}>
         {!showCountDown ? 
-        <VStack flex={1} marginHorizontal={12} marginBottom={32}>
-            <VStack flex={1} alignItems='center' marginVertical={12}>
-                <VStack marginHorizontal={24}>
-                    <HStack alignItems='center'>
-                        <SVG xml={ear} width="32" height="32" />
-                        <Text style={Typography.heading.h2}> Tutorial</Text>
+        <VStack flex={1} marginBottom={8}>
+            <VStack flex={1} alignItems='center'>
+                <VStack>
+                    <HStack alignItems='center' justifyContent="space-between">
+                        <HeaderText text="Tutorial" xml={testEar} underlineColor={Colors.primary.p5} />
+                        <CloseButton navigation={navigation} section={"Noise Check"} />
                     </HStack>
                     <Text style={styles.tutorialText}>Press and Hold the Button while you hear the beeping sound.</Text>
                 </VStack>
-                <View style={{flex:1, justifyContent: 'center'}}>
-                    <TouchableOpacity style={{width:116, height: 116, borderRadius: 58, backgroundColor: Colors.primary.p2, justifyContent:'center', alignItems: 'center'}}>
-                        <SVG xml={testIcon} width="48" height="48" fill={Colors.primary.p5} />
-                    </TouchableOpacity>
-                </View>
+                <VStack flex={1} justifyContent= 'center' alignItems='center'>
+                    {btnAnimate ?
+                    <> 
+                    <AnimatedLottieView source={require('../animation/ButtonBackgroundRipple.json')} autoPlay style={{width:300, height: 300, position: 'absolute'}} />
+                    <SVG xml={heldTestBtn} width={150} height={150} />
+                    </> :
+                    <SVG xml={testBtn} width={150} height={150} />}
+                    <AnimatedLottieView source={require('../animation/Tap&Hold.json')} autoPlay style={{width:300, height: 300, position: 'absolute', bottom:80}} />
+                </VStack>
             </VStack>
             <View style={{marginHorizontal: 12}}>
                 <ButtonFunc text="Start Test" handleOnPress={startCountDown} />
@@ -56,7 +80,7 @@ const TestTutorial = ({navigation}) => {
             <Text style={Typography.heading.h1}>{state.count}</Text>
         </VStack>
         }
-        </>
+        </VStack>
     );
 };
 
@@ -74,7 +98,7 @@ const styles = StyleSheet.create({
       ...Typography.heading.h3
   },
   tutorialText: {
-      marginTop: 16,
-      ...Typography.body.bxl
+      marginVertical: 32,
+      ...Typography.body.bxl,
   }
 });
